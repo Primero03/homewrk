757fcb434aace30c3898525cdfa2f8bb40b6ac0b
Primero03 <116756259+Primero03@users.noreply.github.com>
2026-04-11 20:21:12 +0300

    Author and Book

---
+++ hw3/Author.java
@@ -0,0 +1,18 @@
+package hw.hw3;
+
+public class Author {
+    public String name;
+    public String surname;
+    public int reting;
+
+    public Author(String name, String surname, int reting){
+        this.name = name;
+        this.surname = surname;
+        this.reting = reting;
+    }
+
+    public String toString(){
+        return "[ " + name + " " + surname + " (" + reting + ") ]" ;
+    }
+
+}
