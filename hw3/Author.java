3089d36d63f908a978bb58ea986038f5715ef768
Primero03 <116756259+Primero03@users.noreply.github.com>
2026-04-11 20:48:41 +0300

    reformat Author.java

--- hw3/Author.java
+++ hw3/Author.java
@@ -5,14 +5,14 @@ public class Author {
     public String surname;
     public int reting;
 
-    public Author(String name, String surname, int reting){
+    public Author(String name, String surname, int reting) {
         this.name = name;
         this.surname = surname;
         this.reting = reting;
     }
 
-    public String toString(){
-        return "[ " + name + " " + surname + " (" + reting + ") ]" ;
+    public String toString() {
+        return "[ " + name + " " + surname + " (" + reting + ") ]";
     }
 
 }
