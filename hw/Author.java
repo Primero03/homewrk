44d2c8c842a3e0593ef81c713e95305f0cbf0950
Primero03 <116756259+Primero03@users.noreply.github.com>
2026-04-11 20:24:59 +0300

    Update Author.java

--- hw/Author.java
+++ hw/Author.java
@@ -1,18 +1,1 @@
-package hw;
 
-public class Author {
-    public String name;
-    public String surname;
-    public int reting;
-
-    public Author(String name, String surname, int reting){
-        this.name = name;
-        this.surname = surname;
-        this.reting = reting;
-    }
-
-    public String toString(){
-        return "[ " + name + " " + surname + " (" + reting + ") ]" ;
-    }
-
-}
