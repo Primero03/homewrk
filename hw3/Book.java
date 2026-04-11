52afa67ba5abd39a3f1a46756af7155ba665842a
Primero03 <116756259+Primero03@users.noreply.github.com>
2026-04-11 20:47:11 +0300

    reformat Book.java

--- hw3/Book.java
+++ hw3/Book.java
@@ -4,49 +4,48 @@ public class Book {
     public String title;
     public int releaseYear;
     public Author author;
-    public  int pages;
+    public int pages;
 
 
-    public Book(String title, int releaseYear, Author author, int pages){
+    public Book(String title, int releaseYear, Author author, int pages) {
         this.title = title;
         this.releaseYear = releaseYear;
         this.author = author;
         this.pages = pages;
     }
 
-    public boolean isBig(Book book){
-        if(book.pages > 500){
-           return true;
-       }
-       else {
-           return false;
-       }
+    public boolean isBig(Book book) {
+        if (book.pages > 500) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    public  int estimePrice(Book book){
+    public int estimePrice(Book book) {
         int pricePage;
         pricePage = book.pages * 3;
         //умножение на квадратный корень рейтинга писателя
         double sqrt = Math.sqrt(pricePage);
-        int resultSqrt = (int)Math.floor(sqrt * author.reting);
+        int resultSqrt = (int) Math.floor(sqrt * author.reting);
 
         return resultSqrt;
 
     }
 
-    public boolean matches(Book book,Author author,String word){
+    public boolean matches(Book book, Author author, String word) {
 
-        if(
-        book.title.contains(word) || author.name.contains(word) || author.surname.contains(word) == true){
+        if (
+                book.title.contains(word) || author.name.contains(word) || author.surname.contains(word) == true) {
             return true;
-        }else{
-        return false;
+        } else {
+            return false;
         }
     }
 
 
-    public String toString(){
-        return "[ " + title +", "+ releaseYear +"г."+ " ] - " + author + "  " + pages + " стр. ";
+    public String toString() {
+        return "[ " + title + ", " + releaseYear + "г." + " ] - " + author + "  " + pages + " стр. ";
     }
 
 }
