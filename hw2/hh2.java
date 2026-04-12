d52b0d6b52c35026aeecc3c6772915adae068ca3
Primero03 <116756259+Primero03@users.noreply.github.com>
2026-04-12 14:35:48 +0300

    HW_FIRST

---
+++ hw2/hh2.java
@@ -0,0 +1,15 @@
+package taxes;
+
+class hh2 {
+
+    static void main() {
+        System.out.println("Denis Gladkov");
+        int income = 44000;
+        int spending = 30000;
+        System.out.println("Остаток средств на счету: " + (income-spending) + " руб.");
+    }
+}
+
+
+
+
