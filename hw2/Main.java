5bd1f14e776378f9182f257b560be8bac96771d7
Primero03 <116756259+Primero03@users.noreply.github.com>
2026-04-13 06:25:21 +0300

    Update.java

--- hw2/Main.java
+++ hw2/Main.java
@@ -1,9 +1,10 @@
 package hw2;
 
-class hh2 {
-
-    static void main() {
+public class Main {
+    public static void main(String[] args) {
         System.out.println("Denis Gladkov");
+
+        
         int income = 44000;
         int spending = 30000;
         System.out.println("Остаток средств на счету: " + (income-spending) + " руб.");
